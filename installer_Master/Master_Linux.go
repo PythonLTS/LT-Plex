c5e727a544530c96b27323ca4a40d5dc51a64e0f
LTS <theaghent955@gmail.com>
2026-06-20 11:23:00 +0300

    Master Installer | dev-0-0-5

--- installer_Master/Master_Linux.go
+++ installer_Master/Master_Linux.go
@@ -7,9 +7,18 @@ import (
 	"runtime"
 	"time"
 	"net"
+	"encoding/json"
 )
 
-func get_local_ip()string{
+type Settings struct {
+	  Status 	bool   `json:"status"`
+	  Language  string `json:"language"`
+	  Theme 	string `json:"theme"`
+	  SmartPlex bool   `json:"smartPlex"`
+	  Dns		bool   `json:"dns"`
+}
+
+func getLocalIp() string {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		fmt.Printf("[Ошибка!] не удалось получить интерфейсы: %v", err)
@@ -24,10 +33,10 @@ func get_local_ip()string{
 			}
 		}
 	}
-	return ""
+	return "http://localhost:8080"
 }
 
-func verify_os_name(){
+func verifyOSName(){
 	fmt.Println("# # Проверка ОС # #")
 	if (runtime.GOOS == "linux") {
 		time.Sleep(500 * time.Millisecond)
@@ -41,6 +50,47 @@ func verify_os_name(){
 	return
 }
 
+func makeLTpack(){
+	return
+}
+
+func executeSettings(){
+	return
+}
+
+func savesettings(w http.ResponseWriter,r *http.Request){
+
+	var data Settings
+
+	decoder := json.NewDecoder(r.Body)
+
+	
+	err := decoder.Decode(&data)
+	if err != nil {
+		fmt.Println("ошибка savesettings")
+		return
+	}
+	fmt.Printf("Настройки Пришли!\n:\nФлаг запуска:%t\nЯзык:%s\nТема:%s\nДополнение:%t\nДоменное имя:%t\n",data.Status,data.Language,data.Theme,data.SmartPlex,data.Dns)
+	w.WriteHeader(http.StatusOK)
+}
+
+func storage(w http.ResponseWriter,r *http.Request){
+	return
+}
+
+func storagecapacity(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("100"))
+	return
+}
+
+func storagestatus(w http.ResponseWriter,r *http.Request){
+	return
+}
+
+func updateapp(w http.ResponseWriter,r *http.Request){
+	return
+}
+
 func root(w http.ResponseWriter,r *http.Request){
 	http.ServeFile(w,r,"master_resources/Welcome.html")
 	return
@@ -48,12 +98,18 @@ func root(w http.ResponseWriter,r *http.Request){
 
 func main(){
 	fmt.Println("### Инициализация ###")
-	verify_os_name()
+	verifyOSName()
 	fmt.Println("Проверка прошла успешно!")
 	time.Sleep(500 * time.Millisecond)
 	fmt.Println("### Запуск установщика ###")
-	fmt.Println("Успешно!\nОткройте в браузере сайт :\n",get_local_ip())
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("Успешно!\nОткройте в браузере сайт :\n",getLocalIp())
 	http.Handle("/sources/",http.StripPrefix("/sources/",http.FileServer(http.Dir("master_resources/"))))
+	http.HandleFunc("/saveSettings", savesettings)
+	http.HandleFunc("/UploadPack", storage)
+	http.HandleFunc("/Storage", storagestatus)
+	http.HandleFunc("/StorageCapacity", storagecapacity)
+	http.HandleFunc("/Update", updateapp)
 	http.HandleFunc("/",root)
 	http.ListenAndServe(":8080",nil)
 }
